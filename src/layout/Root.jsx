282a801d68e6b1140d3ea7c1393763bf8c7ed844
zahid072 <jahidhasan19368@gmail.com>
2024-04-25 16:46:11 +0600

    add user methods added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../pages/navbar/Navbar'
 
 const Root = () => {
   return (
     <div>
+        <div>
+            <Navbar/>
+        </div>
+      <div>
       <Outlet/>
+      </div>
     </div>
   )
 }
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
-import Root from '../layout/Root'
-import UsersList from '../pages/usersList/UsersList'
-import AddUser from '../pages/addUser/AddUser'
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+import UsersList from "../pages/usersList/UsersList";
+import AddUser from "../pages/addUser/AddUser";
 
 const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<Root/>,
-        children:[
-            {
-                path:"/",
-                element:<UsersList/>
-            },
-            {
-                path:"/addUser",
-                element:<AddUser/>
-            },
-            {
-                path:"/updateUser/:id",
-                element:<AddUser/>
-            },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <UsersList />,
+      },
+      {
+        path: "/addUser",
+        element: <AddUser />,
+      },
+      {
+        path: "/updateUser/:id",
+        element: <AddUser />,
+      },
+    ],
+  },
+]);
 
-export default router
+export default router;
